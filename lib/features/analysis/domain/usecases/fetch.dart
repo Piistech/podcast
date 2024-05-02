b65f9b5f487302c5197d4b3962aaeea957abb4c1
Omar Khaium Chowdhury <omar.piistech@gmail.com>
2024-05-02 17:03:16 +0600

    Add FetchAnalysisUseCase and FindAnalysisByIdUseCase for fetching and finding analysis data

---
+++ lib/features/analysis/domain/usecases/fetch.dart
@@ -0,0 +1,18 @@
+import 'package:either_dart/either.dart';
+
+import '../../../../core/shared/shared.dart';
+import '../../analysis.dart';
+
+class FetchAnalysisUseCase {
+  final AnalysisRepository repository;
+
+  FetchAnalysisUseCase({
+    required this.repository,
+  });
+
+  Future<Either<Failure, AnalysisEntity>> call({
+    required String fixtureGuid,
+  }) async {
+    return await repository.fetch(fixtureGuid: fixtureGuid);
+  }
+}
